6d75a91aaf356e72398627ac86fdac9d09bba9a9
Frans Chokoe <fmoyagabo@gmail.com>
2026-01-03 00:23:02 +0200

    mini db

---
+++ mini.db.py
@@ -0,0 +1,12 @@
+students = {
+    1:{
+        "name": "Frans",
+        "age": 23,
+        "class": "year 2022"
+    },
+    4:{
+        "name": "Mmatema",
+        "age":27,
+        "Song": "O tshepegile morena"
+    }
+}
